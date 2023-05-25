99ca1f34b6e09b2cbe76a221edb81363e6f740c5
김한성 <anjfkrhdkdle@naver.com>
2023-05-25 10:10:39 +0900

    chore : 디자인 수정

--- frontend/src/components/plan/PlanListItem.vue
+++ frontend/src/components/plan/PlanListItem.vue
@@ -2,17 +2,18 @@
     <!-- Plan Card Start -->
     <b-col lg="3" md="4" sm="12" class="mb-2 p-3 col-sm card-item">
         <b-card
+            style="min-width: 250px;"
             :img-src="tripPlan.imgPath"
             img-alt="Image"
             img-top
             img-height="200px"
-            img-width="100%"
+            img-width="auto"
             tag="article"
             class="mb-2">
             
             <div class="d-flex flex-column">
-                <p class="font-weight-bold" style="font-size: 0.9rem">{{ tripPlan.title }}</p>
-                <b-card-text style="font-size: 0.8rem"> {{ tripPlan.startDate }} </b-card-text>
+                <p class="font-weight-bold" style="font-size: 1.2rem">{{ tripPlan.title }}</p>
+                <b-card-text style="font-size: 0.9rem"> {{ tripPlan.startDate }} </b-card-text>
                 <b-button @click="movePlanDetail" class="button">플랜 보기</b-button>
             </div>
         </b-card>
@@ -49,8 +50,8 @@ export default {
 }
 .button {
     border: none;
-    background-color: lightblue;
-    color:black;
+    background-color: #62aff6;
+    color:white;
     font-weight: bold;
 }
 </style>
